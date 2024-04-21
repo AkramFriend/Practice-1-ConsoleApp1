ea5ff1332f577353c85daaa89d893fbac70fdf7c
Акрам <@LAPTOP-36FLJ062>
2024-04-22 01:20:54 +0300

    Добавьте файлы проекта.

---
+++ Practice(1.2)ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_1._2_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello,");
+            Console.Write(" World");
+            Console.Write("!!!");
+            Console.ReadLine();
+        }
+    }
+}
